6b6cd5febbd8c5961171a2b16961f8b6fd79df30
Duke4Coco <u531156798@byui.edu>
2022-12-15 08:08:46 +0200

    Deleted some files and renamed one file.

--- sample.py
+++ sample.py
@@ -1,19 +1,17 @@
-Create database Electronics;
-
 
 # Import the sleep function from the time module, so
 # that the sleep function can be used in this program.
-# from time import sleep
+from time import sleep
 
 #  Prompt the user to enter thier name.
-# name = input("Hello! What is your name? ").capitalize()
+name = input("Hello! What is your name? ").capitalize()
 
 #  Print the numbers 3, 2, 1.
-# for i in range(3, 0, -1):
-#     print(i, flush=True)
-#     sleep(0.5)  # Pause for 1/2 second.
+for i in range(3, 0, -1):
+    print(i, flush=True)
+    sleep(0.5)  # Pause for 1/2 second.
 
 #  Use a Python f-string to format a greeting
 #  for the user and then print the greeting.
-# print(f"Welcome to CSE 111, {name}!")
+print(f"Welcome to CSE 111, {name}!")
 
--- select.sql
+++
@@ -1,3 +0,0 @@
--- Example 2
-
-SELECT AVG(numbers) FROM table;
